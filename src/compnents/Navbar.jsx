d9877816a2788c2b072f184f6e31b021aa0b7758
Yogesh-Jugwan <yogeshjugwan@gmail.com>
2021-10-24 21:58:39 +0530

    navbar update

--- src/compnents/Navbar.jsx
+++ src/compnents/Navbar.jsx
@@ -8,19 +8,19 @@ export default function Navbar(props) {
     <>
       <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">Navbar</Link>
+          <Link className="navbar-brand" to="/">Text_Formate</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="#">Home</Link>
+                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
               </li>
               <li className="nav-item">
                 <Link className="nav-link" to="/about">About</Link>
               </li>
-              <li className="nav-item dropdown">
+              {/* <li className="nav-item dropdown">
                 <Link className="nav-link dropdown-toggle" to="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Dropdown
                 </Link>
@@ -30,7 +30,7 @@ export default function Navbar(props) {
                   <li><hr className="dropdown-divider" /></li>
                   <li><Link className="dropdown-item" to="#">Something else here</Link></li>
                 </ul>
-              </li>
+              </li> */}
             
             </ul>
             <div class={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
